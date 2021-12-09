bf255b3cc9974ec3774b10ae2f15fe31ab29696b
Fernando Tzoc <fernandotzoc4@gmail.com>
2021-12-08 23:51:43 -0600

    Install Script Added

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Removing existing dotfiles"
+# remove files if they already exist
+unlink ~/.config/nvim
+rm -rf ~/.config/nvim/
+
+echo "Creating symlinks"
+# Neovim expects some folders already exist
+mkdir -p ~/.config
+
+# Symlinking files
+ln -s ~/dotfiles/nvim/ ~/.config/
+
+
+#SSH Settings
+mkdir -p ~/.ssh && chmod 700 ~/.SSH
+touch ~/.ssh/config
+chmod 600 ~/.ssh/config
